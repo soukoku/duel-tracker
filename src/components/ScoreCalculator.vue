28b2fb28e3b9c9e8dd21fb431bc76eb050704694
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-01-13 22:13:21 -0500

    Add tailwind

---
+++ src/components/ScoreCalculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="calculator">
+    {{ score }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      score: 8000
+    }
+  },
+  methods: {}
+}
+</script>
