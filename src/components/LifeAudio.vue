4cecb9bef921b538eda660b4987390bc545d76e6
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-01-15 18:52:37 -0500

    Made audio volume a global state.

--- src/components/LifeAudio.vue
+++ src/components/LifeAudio.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="hidden">
-    <audio preload ref="start">
+    <audio preload="auto" ref="start">
       <source src="sounds/duel-start.ogg" type="audio/ogg; codecs=vorbis" />
       <source src="sounds/duel-start.mp3" type="audio/mpeg" />
     </audio>
-    <audio preload ref="change">
+    <audio preload="auto" ref="change">
       <source src="sounds/life-change.ogg" type="audio/ogg; codecs=vorbis" />
       <source src="sounds/life-change.mp3" type="audio/mpeg" />
     </audio>
-    <audio preload ref="zero">
+    <audio preload="auto" ref="zero">
       <source src="sounds/life-zero.ogg" type="audio/ogg; codecs=vorbis" />
       <source src="sounds/life-zero.mp3" type="audio/mpeg" />
     </audio>
@@ -16,20 +16,31 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 function stopOne(el) {
   el.pause()
   el.currentTime = 0
 }
-function each(arr, act) {
-  for (let i = 0; i < arr.length; i++) act(arr[i])
+function each(nodes, act) {
+  for (let i = 0; i < nodes.length; i++) act(nodes[i])
 }
 
 export default {
+  computed: mapState(['volume']),
+  watch: {
+    volume(val) {
+      this.updateVolume()
+    }
+  },
   mounted() {
-    const nodes = this.$el.querySelectorAll('audio')
-    each(nodes, a => (a.volume = 0.2))
+    this.updateVolume()
   },
   methods: {
+    updateVolume() {
+      const nodes = this.$el.querySelectorAll('audio')
+      each(nodes, a => (a.volume = this.volume))
+    },
     stop() {
       const nodes = this.$el.querySelectorAll('audio')
       each(nodes, stopOne)
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    volume: 1
   },
   mutations: {
+    SET_VOLUME(state, value) {
+      state.volume = value
+    }
   },
-  actions: {
-  },
-  modules: {
-  },
-});
+  actions: {},
+  modules: {}
+})
