bcc1ca097c848e86bed815b157b9821646e7081b
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-01-16 20:04:42 -0500

    Added common value buttons.

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,22 +1,51 @@
 <template>
   <div class="p-4 border-2 border-gray-600">
     <LifeAudio ref="sounds" />
-    <LifePoints :points="points" :animate="!isNew" />
+    <LifePoints :points="points" :animate="!isNew" class="mb-4" />
 
-    <div class="flex">
-      <DButton @click="multiply(0.5)" color="red">
-        Half
-      </DButton>
-      <DButton @click="multiply(2)" color="green">
-        Double
-      </DButton>
-      <DButton @click="add(-100)" disabled color="red">
+    <div class="flex button-row">
+      <DButton @click="add(-100)" color="red">
         -100
       </DButton>
-      <DButton @click="reset">
-        Reset
+      <DButton @click="add(-200)" color="red">
+        -200
+      </DButton>
+      <DButton @click="add(-500)" color="red">
+        -500
+      </DButton>
+      <DButton @click="add(-1000)" color="red">
+        -1000
+      </DButton>
+      <DButton @click="add(-2000)" color="red">
+        -2000
+      </DButton>
+      <DButton @click="multiply(0.5)" color="red">
+        1/2
       </DButton>
     </div>
+    <div class="flex button-row">
+      <DButton @click="add(100)" color="green">
+        +100
+      </DButton>
+      <DButton @click="add(200)" color="green">
+        +200
+      </DButton>
+      <DButton @click="add(500)" color="green">
+        +500
+      </DButton>
+      <DButton @click="add(1000)" color="green">
+        +1000
+      </DButton>
+      <DButton @click="add(2000)" color="green">
+        +2000
+      </DButton>
+      <DButton @click="multiply(2)" color="green">
+        x2
+      </DButton>
+    </div>
+    <DButton @click="reset">
+      Reset
+    </DButton>
   </div>
 </template>
 
@@ -59,3 +88,9 @@ export default {
   }
 }
 </script>
+<style>
+.button-row > button {
+  flex-grow: 1;
+  flex-basis: 0;
+}
+</style>
