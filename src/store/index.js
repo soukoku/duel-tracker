73f7e42043d81ce17c4bfb4637f02b02a58dc07a
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-01-17 21:17:08 -0500

    Persist state in local storage.

--- src/store/index.js
+++ src/store/index.js
@@ -3,21 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const state = {
+  volume: 1,
+  defaultLife: 8000,
+  defaultPlayers: 1,
+  ...JSON.parse(localStorage.getItem('dt-state') || '{}')
+}
+
+function persist(state) {
+  // explode state to keep only props
+  localStorage.setItem('dt-state', JSON.stringify({ ...state }))
+}
+
 export default new Vuex.Store({
-  state: {
-    volume: 1,
-    defaultLife: 8000,
-    defaultPlayers: 1
-  },
+  state: state,
   mutations: {
     SET_VOLUME(state, value) {
       state.volume = value
+      persist(state)
     },
     SET_DEFAULTLIFE(state, value) {
       state.defaultLife = value
+      persist(state)
     },
     SET_DEFAULTPLAYER(state, value) {
       state.defaultPlayers = value
+      persist(state)
     }
   },
   actions: {},
