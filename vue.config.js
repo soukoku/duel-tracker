3b87cd9d71e3cb9e2e302fe2d6cf86a3ae6f84e2
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2020-01-19 20:58:06 -0500

    Using api to generate manifest.

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: 'Duel Tracker',
+    themeColor: '#2c5282',
+    msTileColor: '#2c5282',
+    assetsVersion: '1',
+    manifestOptions: {
+      description: 'A Yu-Gi-Oh! life point tracker with sounds.',
+      background_color: '#2c5282',
+      lang: 'en-US',
+      orientation: 'portrait'
+    }
+  }
+}
