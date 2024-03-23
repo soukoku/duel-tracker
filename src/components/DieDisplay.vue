eb8641d7bdd58a01587bab4c007bf05423c7dfc2
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2024-03-23 14:32:05 -0400

    Dice color tweak.

--- src/components/DieDisplay.vue
+++ src/components/DieDisplay.vue
@@ -23,32 +23,32 @@ watch(() => appSettings.volume, val => {
 <template>
   <span class="flex items-center justify-center m-2 die">
     <span class="sr-only">{{ props.value }}</span>
-    <SvgIcon class="face face-1" size="64">
-      <circle cx="12" cy="12" r="4" fill="red" />
+    <SvgIcon class="text-red-600 face face-1" size="64">
+      <circle cx="12" cy="12" r="4" />
     </SvgIcon>
-    <SvgIcon class="face face-2" size="64">
+    <SvgIcon class="text-gray-800 face face-2" size="64">
       <circle cx="6" cy="6" r="2" />
       <circle cx="18" cy="18" r="2" />
     </SvgIcon>
-    <SvgIcon class="face face-3" size="64">
+    <SvgIcon class="text-gray-800 face face-3" size="64">
       <circle cx="6" cy="6" r="2" />
       <circle cx="12" cy="12" r="2" />
       <circle cx="18" cy="18" r="2" />
     </SvgIcon>
-    <SvgIcon class="face face-4" size="64">
+    <SvgIcon class="text-gray-800 face face-4" size="64">
       <circle cx="6" cy="6" r="2" />
       <circle cx="6" cy="18" r="2" />
       <circle cx="18" cy="6" r="2" />
       <circle cx="18" cy="18" r="2" />
     </SvgIcon>
-    <SvgIcon class="face face-5" size="64">
+    <SvgIcon class="text-gray-800 face face-5" size="64">
       <circle cx="6" cy="6" r="2" />
       <circle cx="6" cy="18" r="2" />
       <circle cx="18" cy="6" r="2" />
       <circle cx="18" cy="18" r="2" />
       <circle cx="12" cy="12" r="2" />
     </SvgIcon>
-    <SvgIcon class="face face-6" size="64">
+    <SvgIcon class="text-gray-800 face face-6" size="64">
       <circle cx="6" cy="6" r="2" />
       <circle cx="6" cy="12" r="2" />
       <circle cx="6" cy="18" r="2" />
@@ -66,7 +66,7 @@ watch(() => appSettings.volume, val => {
 .die {}
 
 .face {
-  @apply border-2 border-gray-300 rounded text-black bg-gray-50
+  @apply border-2 border-gray-300 rounded bg-gray-50
 }
 
 .face-1 {}
